562334e31e506e45106a96cc5d2388d6b713cb98
Amber Kim <ambergkim@gmail.com>
2018-06-13 20:51:11 -0700

    server tests

--- index.js
+++ index.js
@@ -9,51 +9,35 @@ const Questions = require('./models/questions.js');
 
 mongoose.connect('mongodb://localhost:27017/qlaunch');
 
+function reorderMessages(messages) {
+  messages.sort((a, b) => {
+    return b.votes - a.votes;
+  });
+  return messages;
+}
+
 io.on('connection', function(socket){
-  // Questions.find({})
-  //   .then(data => {
-  //     console.log('initial load', data);
-  //     data.sort((a, b) => {
-  //       return b.votes - a.votes;
-  //     });
-  //     console.log('sorted', data);
-  //     return data;
-  //   })
-  //   .then(data => {
-  //     socket.emit('send-all-questions', data);
-  //   });
   
   socket.on('send-question', question => {
-    console.log('B1. question received');
 
     Questions.create(question)
       .then(() => {
         return Questions.find({room: question.room});
       })
       .then(questions => {
-        console.log('B2. created questions from db', questions);
-        questions.sort((a, b) => {
-          return b.votes - a.votes;
-        });
-        console.log('B3. sorted questions from db', questions);
-        return questions;
+        return reorderMessages(questions);
       })
       .then(questions => {
-        console.log('B4. questions before sending sorted questions', questions);
         io.to(question.room).emit('send-all-questions', questions);
       });
   });
 
   socket.on('create-room', room => {
-    console.log('user created/joined room');
     socket.join(room);
 
     Questions.find({room: room})
       .then(questions => {
-        questions.sort((a, b) => {
-          return b.votes - a.votes;
-        });
-        return questions;
+        return reorderMessages(questions);
       })
       .then(questions => {
         socket.emit('send-all-questions', questions);
