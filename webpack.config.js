c02a5cc774206af4b7eadf13aecfb0efaa60811c
Amber Kim <ambergkim@gmail.com>
2018-06-20 20:00:05 -0700

    fixed image loading

--- webpack.config.js
+++ webpack.config.js
@@ -29,10 +29,20 @@ const config = {
           loader: 'url-loader',
           options: { 
             limit: 8000, // Convert images < 8kb to base64 strings
-            name: 'images/[hash]-[name].[ext]'
+            name: 'img/[hash]-[name].[ext]'
           } 
         }]
-      }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'img/[name].[ext]',
+            context: ''
+          }
+        },
+      },
     ]
   }
 };
